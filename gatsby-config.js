33ab4de57cbe2226904160d800dcc27c0670c44a
yatfu <55002354+yatfu@users.noreply.github.com>
2023-09-30 14:25:39 -1000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Image To Text`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
